37900e8d7950ef489686bf3474305792bd809b2b
Angeline Tan <atan009@gmail.com>
2016-12-14 18:51:59 +0800

    Add package structure and initial files

---
+++ src/main/com/intro/Diamond.java
@@ -0,0 +1,35 @@
+package main.com.intro;
+
+/**
+ * Created by lnfra on 14/12/16.
+ */
+public class Diamond
+/*
+  Isosceles Triangle
+  Given a number n, print a centered triangle. Example for n=3:
+  *
+  ***
+  *****
+*/
+
+/*
+  Diamond
+  Given a number n, print a centered diamond. Example for n=3:
+  *
+  ***
+  *****
+  ***
+  *
+*/
+
+/*
+  Diamond with Name
+  Given a number n, print a centered diamond with your name in place of the middle line. Example for n=3:
+  *
+  ***
+  Bill
+  ***
+  *
+*/
+
+}
---
+++ src/main/com/intro/Triangle.java
@@ -0,0 +1,40 @@
+package main.com.intro;
+
+/**
+ * Created by lnfra on 14/12/16.
+ */
+public class Triangle {
+
+/*
+  Print one asterisk to the console.
+  Example: *
+*/
+
+
+/*
+  Draw a horizontal line
+  Given a number n, print n asterisks on one line.
+  Example when n=8:    ********
+*/
+
+
+/*
+  Draw a vertical line
+  Given a number n, print n lines, each with one asterisks
+  Example when n=3:
+    *
+    *
+    *
+* /
+
+
+/*
+  Draw a right triangle
+  Given a number n, print n lines, each with one more asterisk than the last (i.e. one on the first line, two on the second,etc.)
+  Example when n=3:
+    *
+    **
+    ***
+*/
+
+}
